2060d4ccdf275d360032751b88d04a7f67827ddd
Craig L. Zirbel <zirbel@bgsu.edu>
2015-11-26 11:54:48 -0500

    Uses biggest piece among all legal moves, randomly

---
+++ player_craig/player_biggest_random.py
@@ -0,0 +1,34 @@
+from game_state import *
+from move import *
+from game_utils import *
+from move_utils import *
+import random
+
+def getMove(state, player):
+
+    oksquares = getLegalSquares(state,player)
+
+    print("  > Found "+str(len(oksquares))+" OK squares to occupy")
+
+    okmoves = getLegalMoves(state,player,oksquares)
+
+    print("  > Found "+str(len(okmoves))+" legal moves")
+
+    mymove = None
+
+    if len(okmoves) > 0:
+      biggestsize = 0
+      for move in okmoves:
+        cells = move.getNumCells()
+        if cells > biggestsize:
+          biggestsize = cells
+      biggestmoves = []
+      for move in okmoves:
+        cells = move.getNumCells()
+        if cells == biggestsize:
+          biggestmoves.append(move)
+      r = random.randint(0,len(biggestmoves)-1)
+      mymove = biggestmoves[r]
+
+
+    return mymove
